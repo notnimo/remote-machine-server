c12a12ee3cd5bcd2a2aeb05e098a7e1a4e113378
not_nimo <damiano.livoni@gmail.com>
2025-12-02 23:55:32 +0000

    created separated field file for form

---
+++ src/app/login/fields.ts
@@ -0,0 +1,15 @@
+const usernameField = {
+	htmlFor: "username",
+	label: "Username",
+	placeholder: "username",
+	type: "user-name",
+	id: "user-name",
+};
+const passwordField = {
+	htmlFor: "password",
+	label: "Password",
+	placeholder: "password",
+	type: "password",
+	id: "password",
+};
+export { usernameField, passwordField };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,13 @@
 import { Card } from "@/src/components/card";
-
 import {
 	FormHeaderGroup,
 	FormSubmitGroup,
 	FormInputGroup,
 } from "@/src/ui/form-components";
 
+import { usernameField, passwordField } from "@/src/app/login/fields";
+
 export default function LoginCard() {
-	const usernameField = {
-		htmlFor: "username",
-		label: "Username",
-		placeholder: "username",
-		type: "user-name",
-		id: "user-name",
-	};
-	const passwordField = {
-		htmlFor: "password",
-		label: "Password",
-		placeholder: "password",
-		type: "password",
-		id: "password",
-	};
 	return (
 		<div className="flex h-screen flex-col items-center justify-center gap-6 p-4">
 			<Card className="w-full max-w-sm bg-black">
