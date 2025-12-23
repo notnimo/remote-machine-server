1dfdfa36a68dc723783d1e82d6d521e5d8bc63cb
not_nimo <damiano.livoni@gmail.com>
2025-12-23 20:45:15 +0100

    rolled back on recent machines to add skeleton while fetching

--- src/ui/recent-machines/recent-machine.tsx
+++ src/ui/recent-machines/recent-machine.tsx
@@ -1,5 +1,3 @@
-import { RecentMenu } from "@/src/ui/recent-machines/recent-machine-menu";
-
 const fetchMachines = () => {
 	return ["stringify", "deinfie"];
 }; //placeholder
@@ -10,5 +8,47 @@ export function RecentMachineSection() {
 		<>
 			<hr /> <h1>Recent Machines</h1> <RecentMenu machines={recent_machines} />
 		</>
+	);
+}
+
+export function RecentMenu({ machines }: { machines: string[] }) {
+	let temp;
+	for (let machineID in machines) {
+		temp = (
+			<>
+				{temp} <MachineCard machineID={machines[machineID]} />
+			</>
+		);
+	}
+	return temp;
+}
+
+import { Trash, ScreenShare } from "lucide-react";
+
+import { Button } from "@/src/components/button";
+import {
+	Item,
+	ItemActions,
+	ItemContent,
+	ItemTitle,
+} from "@/src/components/item";
+
+export function MachineCard({ machineID }: { machineID: string }) {
+	return (
+		<div className="flex w-full max-w-md flex-col gap-6">
+			<Item variant="outline" className="min-w-2xl">
+				<ItemContent>
+					<ItemTitle>{machineID}</ItemTitle>
+				</ItemContent>
+				<ItemActions>
+					<Button variant="outline" size="sm">
+						<Trash />
+					</Button>
+					<Button variant="outline" size="sm">
+						<ScreenShare />
+					</Button>
+				</ItemActions>
+			</Item>
+		</div>
 	);
 }
