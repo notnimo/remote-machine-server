c1252e4d22e7716058d735c3a83aa7f885d9cfb7
not_nimo <damiano.livoni@gmail.com>
2026-01-07 13:05:08 +0000

    vertical hr styled

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,27 +7,19 @@ import { RegisterUser } from "@/src/ui/home/register-user";
 import { UpdateUser } from "@/src/ui/home/update-user";
 
 export default function Page() {
-	/*return (
-		<main className="flex flex-col h-screen">
-			<div className="flex flex-row justify-around p-24">
-				<AddMachine /> <RegisterMachine /> <UpdateMachine /> <RegisterUser />
-				<UpdateUser />
-			</div>
-
-			<hr className="h-1 max-w-4xl rounded-sm" />
-		</main>
-	);*/
-
 	return (
 		<div className="flex h-screen justify-around">
 			<div className="text-center min-w-full">
-				<div className="flex flex-row justify-around p-24">
+				<div className="flex flex-row justify-around p-10 max-h-120px">
 					<AddMachine /> <RegisterMachine /> <UpdateMachine /> <RegisterUser />
 					<UpdateUser />
 				</div>
 				<div className="flex justify-center">
 					<hr className="h-1 max-w-5xl w-full bg-neutral-quaternary border-3 rounded-sm" />
 				</div>
+				<br />
+				<br />
+				<div className="w-1.5 h-150 inline-block bg-neutral-quaternary border-3 rounded-sm"></div>
 			</div>
 		</div>
 	);
