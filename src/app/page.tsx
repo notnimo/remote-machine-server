32fb3ea1fe8b50afd24556bccc247670341138f9
not_nimo <damiano.livoni@gmail.com>
2026-01-21 13:05:38 +0000

    fixed issues with dialog tpes, to fix select field component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,3 @@
-"use client";
-
-import Link from "next/link";
-
-import { RegisterMachine } from "@/src/ui/home/register-machine";
-import { RegisterUser } from "@/src/ui/home/register-user";
-import { UpdateUser } from "@/src/ui/home/update-user";
-import { UpdateMachine } from "@/src/ui/home/update-machine";
-import { HomeButton } from "@/src/ui/home-btn";
-
-import { SideBar } from "@/src/ui/machine-page/sidebar";
-
-/*export default function HomePage() {
-	return (
-		<main>
-			<div className="flex-row items-center justify-center">
-				<RegisterMachine /> <br />
-				<RegisterUser /> <br />
-				<UpdateUser /> <br />
-				<UpdateMachine /> <br />
-				<HomeButton /> <br />
-				<p>
-					<Link href={`/1a2b3c/machine-page/terminal`}>Link</Link>
-				</p>
-			</div>
-		</main>
-	);
-}*/
-
-export default function HomePage() {
-	return (
-		<main>
-			<SideBar />
-		</main>
-	);
+export default function WelcomePage() {
+	return <div>Welcome!</div>;
 }
